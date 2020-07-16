8ade273c07e4438519a33430eb24bbd74394f3d5
Dominik Heidler <dominik@heidler.eu>
2020-07-16 23:38:06 +0200

    Support ADIF format

--- sotareport.py
+++ sotareport.py
@@ -49,6 +49,7 @@ def update_db(name, url, dbfile):
 		file_last_updated = datetime.datetime.fromtimestamp(0)
 	if parsedate(r.headers['last-modified']) > file_last_updated.replace(tzinfo=datetime.timezone.utc):
 		print('Updating %s cache ' % name, end='')
+		sys.stdout.flush()
 		r = requests.get(url, allow_redirects=True, stream=True)
 		with open(dbfile, 'wb') as f:
 			for chunk in r.iter_content(1024*1024):
@@ -57,12 +58,19 @@ def update_db(name, url, dbfile):
 				f.write(chunk)
 			sys.stdout.write("\n")
 
-
 def load_summit_db():
 	f = open(SUMMIT_DB_FILE, 'r')
 	f.readline() # skip first line that looks like "SOTA Summits List (Date=29/06/2020)"
+	print('Loading summits ', end='')
+	sys.stdout.flush()
+	i = 0
 	for summit in csv.DictReader(f):
 		summits[summit['SummitCode']] = summit
+		i += 1
+		if i % 10000 == 0:
+			sys.stdout.write('.')
+			sys.stdout.flush()
+	sys.stdout.write("\n")
 
 def load_name_db():
 	try:
@@ -77,6 +85,37 @@ def summit_distance(summit1, summit2):
 	s1 = summits[summit1]
 	s2 = summits[summit2]
 	return geo_distance(float(s1['Latitude']), float(s1['Longitude']), float(s2['Latitude']), float(s2['Longitude']))
+
+def summit_locator(summit):
+	s = summits[summit]
+	lat = float(s['Latitude'])
+	lon = float(s['Longitude'])
+	precision = 3
+
+	A = ord("A")
+	a = divmod(lon + 180, 20)
+	b = divmod(lat + 90, 10)
+	maiden = chr(A + int(a[0])) + chr(A + int(b[0]))
+	lon = a[1] / 2.0
+	lat = b[1]
+	i = 1
+	while i < precision:
+		i += 1
+		a = divmod(lon, 1)
+		b = divmod(lat, 1)
+		if not (i % 2):
+			maiden += str(int(a[0])) + str(int(b[0]))
+			lon = 24 * a[1]
+			lat = 24 * b[1]
+		else:
+			maiden += chr(A + int(a[0])) + chr(A + int(b[0]))
+			lon = 10 * a[1]
+			lat = 10 * b[1]
+
+	if len(maiden) >= 6:
+		maiden = maiden[:4] + maiden[4:6].lower() + maiden[6:]
+
+	return maiden
 
 def print_line():
 	print('#'*70)
@@ -144,7 +183,7 @@ def query_qso(default={'time': '', 'remote_callsign': '', 'freq': None, 'mode': 
 	default['freq'] = freq if default['freq'] == None else default['freq']
 	time = input_time(strpad('Time (HHMM - UTC): '), default['time'])
 	remote_callsign = input_callsign(strpad('Callsign: '), default['remote_callsign'])
-	freq = rlinput(strpad('Freq (7MHz/21MHz): '), default['freq']).lower().replace('mhz', 'MHz').replace(' ', '')
+	freq = rlinput(strpad('Freq (3.5/7/21): '), default['freq']).lower().replace('mhz', '').replace(' ', '')
 	mode = rlinput(strpad('Mode (CW/SSB/FM): '), default['mode']).upper()
 	rst_gvn = "" if args.no_rst else rlinput(strpad('RST given: '), default['rst_gvn']).upper()
 	rst_rec = "" if args.no_rst else rlinput(strpad('RST received: '), default['rst_rec']).upper()
@@ -180,10 +219,55 @@ def write_csv(filename, mode):
 			comment = ', '.join(comments)
 
 			# [V2] [My Callsign][My Summit] [Date] [Time] [Band] [Mode] [His Callsign] [His Summit] [Notes or Comments]
-			csvfile.writerow(['V2', callsign, summit, date.strftime('%d/%m/%y'), l['time'].strftime('%H:%M'), l['freq'], l['mode'], l['remote_callsign'], l['remote_summit'], comment])
+			csvfile.writerow(['V2', callsign, summit, date.strftime('%d/%m/%y'), l['time'].strftime('%H:%M'), l['freq']+'MHz', l['mode'], l['remote_callsign'], l['remote_summit'], comment])
+
+def write_adi(filename, mode):
+	with open(filename, mode+'b') as f:
+		import hamutils.adif
+		import hamutils.adif.common
+		adi = hamutils.adif.ADIWriter(f)
+		for l in log:
+			qso = {}
+			qso['operator'] = callsign
+			if summit:
+				qso['my_sota_ref'] = summit
+				qso['my_sig_info'] = "%(SummitName)s (%(AltM)sm), %(RegionName)s, %(AssociationName)s" % summits[summit]
+				qso['my_gridsquare'] = summit_locator(summit)
+				qso['my_lat'] = float(summits[summit]['Latitude'])
+				qso['my_lon'] = float(summits[summit]['Longitude'])
+			if rig:
+				qso['my_rig'] = rig
+			if antenna:
+				qso['my_antenna'] = antenna
+			qso['qso_date'] = date
+			qso['time_on']= l['time']
+			qso['freq'] = float(l['freq'])
+			qso['band'] = hamutils.adif.common.convert_freq_to_band(qso['freq']) or "%im" % (300/qso['freq'])
+			qso['mode'] = l['mode']
+			qso['call'] = l['remote_callsign']
+			if l['remote_summit']:
+				qso['sota_ref'] = l['remote_summit']
+				qso['sig_info'] = "%(SummitName)s (%(AltM)sm), %(RegionName)s, %(AssociationName)s" % summits[l['remote_summit']]
+				qso['gridsquare'] = summit_locator(l['remote_summit'])
+				qso['lat'] = float(summits[l['remote_summit']]['Latitude'])
+				qso['lon'] = float(summits[l['remote_summit']]['Longitude'])
+				if summit:
+					qso['distance'] = round(summit_distance(summit, l['remote_summit']), 2)
+			qso['rst_rcvd'] = l['rst_rec']
+			qso['rst_sent'] = l['rst_gvn']
+			qso['comment'] = l['comment']
+			adi.add_qso(**qso)
+		adi.close()
 
 def update_backup():
-	write_csv('.'+args.output_file+'.bak', 'w')
+	write_outputfile(args.output_file, '.bak', 'w')
+
+def write_outputfile(fname, fsuffix='', mode='w'):
+	output_file = fname + fsuffix
+	if fname.endswith('.csv'):
+		write_csv(output_file, mode)
+	elif fname.endswith('.adi'):
+		write_adi(output_file, mode)
 
 def command_handler():
 	print("\x1b[2K\rAvailable Commands:")
@@ -207,8 +291,8 @@ def command_handler():
 			elif cmd == 'D':
 				import pdb; pdb.set_trace()
 			elif cmd == 'S':
-				write_csv(args.output_file, 'a')
-				os.unlink('.'+args.output_file+'.bak')
+				write_outputfile(args.output_file, mode='a')
+				os.unlink(args.output_file+'.bak')
 				sys.exit(0)
 		except KeyboardInterrupt:
 			input('Press Ctrl+C to quit without saving - or press ENTER to return to CMD prompt.')
@@ -233,6 +317,8 @@ if summit:
 	print(strpad('Found Summit: ') + "%(SummitName)s (%(AltM)sm), %(RegionName)s, %(AssociationName)s" % summits[summit])
 else:
 	print(strpad('No summit: ') + 'Assuming chaser')
+rig = input(strpad('RIG: '))
+antenna = input(strpad('Antenna: '))
 date = input_date(strpad('Date (DD.MM.YYYY): '))
 print_line()
 
